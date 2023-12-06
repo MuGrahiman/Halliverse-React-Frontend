107d0976373ac81ae0a51656ca529f9075a980a3
MuGrahiman <mujeebrahiman2000@gmail.com>
2023-12-07 00:12:51 +0530

    change base url

--- src/Store/Apis.js
+++ src/Store/Apis.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: `https://youtube-clone-mug-de178feea9da.herokuapp.com/`,
-  baseURL: "http://localhost:4400/api",
+  baseURL: `https://halliverse-backend.onrender.com/api`,
+  // baseURL: "http://localhost:4400/api",
 });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile"))
